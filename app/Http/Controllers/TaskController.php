2128e0a7c6ec5034cf6d746ca6a53c7663b8dfcf
Carlos Ramirez <carlosjramirez11@gmail.com>
2022-06-13 16:42:18 -0700

    index now loops through the db and outputs the tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\task;
+
 class TaskController extends Controller
 {
     //Returns index file
     public function index()
     {
-        return view('tasks.index');
+        //Querries for all records and retrieves them by date order
+        $tasks = task::latest()->get();
+
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
 
     }
 
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.layout')
 
 @section('content')
-
+<div class="container">
+    <div class="row">
+        @foreach ($tasks as $task)
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{{$task->title}}</h5>
+                    <p class="card-text">{{$task->description}}</p>
+                    <span class="card-text">{{$task->status}}</span>
+                    <span class="card-text">{{$task->dueDate}}</span>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 @endsection
