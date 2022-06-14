6f2eb2e9ad6cf274abb6fca7908713d734aa8c6f
Carlos Ramirez <carlosjramirez11@gmail.com>
2022-06-13 19:18:18 -0700

    Container div for index now spands the whole view port

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row">
         @foreach ($tasks as $task)
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">{{$task->title}}</h5>
                     <p class="card-text">{{$task->description}}</p>
-                    <span class="card-text">{{$task->status}}</span>
-                    <span class="card-text">{{date('m-d-Y', strtotime($task->dueDate))}}</span>
+                    <div class="row">
+                        <div class="card-text col">{{$task->status}}</div>
+                        <div class="card-text col text-end">{{date('m/d/y', strtotime($task->dueDate))}}</div>
+                    </div>
                 </div>
             </div>
         @endforeach
