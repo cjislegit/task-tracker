bd66383b34edd04859a80759b97c06ddb17aac2f
Carlos Ramirez <carlosjramirez11@gmail.com>
2022-06-14 20:52:58 -0700

    Created simple form for updating the task

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.layout')
 
 @section('content')
-
+<div class="container">
+<form action="">
+    <div class="mb-3">
+        <label for="title" class="form-label">Title</label>
+        <input type="text" id="title" class="form-control" value="{{$task->title}}">
+    </div>
+    <div class="mb-3">
+        <label class="form-label" for="status">Task Status</label>
+        <select class="form-select form-select" aria-label=".form-select-sm example" id="status">
+            <option {{$task->status === 'complete' ? 'selected' : null}} value="complete">Complete</option>
+            <option {{$task->status === 'progress' ? 'selected' : null}} value=progress">In Progress</option>
+        </select>
+    </div>
+    <div class="mb-3">
+        <label for="taskDescription" class="form-label"> Task Description</label>
+        <textarea name="taskDescription" id="taskDescription" cols="30" rows="10" class="form-control">{{$task->description}}</textarea>
+    </div>
+</form>
+</div>
 @endsection
