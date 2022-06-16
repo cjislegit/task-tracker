5780c4317b36eb8147c26ddbe25f03333a078b02
Carlos Ramirez <carlosjramirez11@gmail.com>
2022-06-15 20:18:45 -0700

    Added a name to all fields and added a sub button

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,22 +2,23 @@
 
 @section('content')
 <div class="container">
-<form action="">
-    <div class="mb-3">
-        <label for="title" class="form-label">Title</label>
-        <input type="text" id="title" class="form-control" value="{{$task->title}}">
-    </div>
-    <div class="mb-3">
-        <label class="form-label" for="status">Task Status</label>
-        <select class="form-select form-select" aria-label=".form-select-sm example" id="status">
-            <option {{$task->status === 'complete' ? 'selected' : null}} value="complete">Complete</option>
-            <option {{$task->status === 'progress' ? 'selected' : null}} value=progress">In Progress</option>
-        </select>
-    </div>
-    <div class="mb-3">
-        <label for="taskDescription" class="form-label"> Task Description</label>
-        <textarea name="taskDescription" id="taskDescription" cols="30" rows="10" class="form-control">{{$task->description}}</textarea>
-    </div>
-</form>
+    <form action="POST">
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input name="title" type="text" id="title" class="form-control" value="{{$task->title}}">
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="status">Task Status</label>
+            <select name="status" class="form-select form-select" aria-label=".form-select-sm example" id="status">
+                <option {{$task->status === 'complete' ? 'selected' : null}} value="complete">Complete</option>
+                <option {{$task->status === 'progress' ? 'selected' : null}} value=progress">In Progress</option>
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="taskDescription" class="form-label"> Task Description</label>
+            <textarea name="description" id="taskDescription" cols="30" rows="10" class="form-control">{{$task->description}}</textarea>
+        </div>
+        <button type="submit" class="btn btn-dark float-end">Submit</button>
+    </form>
 </div>
 @endsection
