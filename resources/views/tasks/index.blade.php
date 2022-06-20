dab46cb7e3f68a488217775478ca98e86aaf5a94
Carlos Ramirez <carlosjramirez11@gmail.com>
2022-06-20 14:42:02 -0700

    Status is now uppercase

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -10,7 +10,7 @@
                             <h5 class="card-title">{{$task->title}}</h5>
                             <p class="card-text">{{$task->description}}</p>
                             <div class="row">
-                                <div class="card-text col">{{$task->status}}</div>
+                                <div class="card-text col status">{{$task->status}}</div>
                                 <div class="card-text col text-end">{{date('m/d/y', strtotime($task->dueDate))}}</div>
                             </div>
                         </div>
